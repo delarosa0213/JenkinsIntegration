7ff3976d91f1ca919dddc2a392f527a04d156a8f
delarosa0213 <rosteldelarosa02@gmail.com>
2023-05-09 14:58:54 +0800

    In this updated version, the Scanner class is used to read input from the user via the command line.
    The user is prompted to enter the value of x and y, and these values are stored in the int variables x and y, respectively. The sum of x and y is then calculated and stored in the int variable ans. Finally, the result is printed to the console using System.out.println().

--- Jenkins_Git_Integration/src/SampleAdd.java
+++ Jenkins_Git_Integration/src/SampleAdd.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
 
 public class SampleAdd {
-	public static void main (String []args) {
-	int y = 10;
-	int x = 12;
-	int ans = x + y;
-	
-	System.out.println(ans);
-	
-	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Enter the value of x: ");
+		int x = scanner.nextInt();
+		
+		System.out.print("Enter the value of y: ");
+		int y = scanner.nextInt();
+		
+		int ans = x + y;
+		
+		System.out.println("The sum of x and y is: " + ans);
+		
+		scanner.close();
 	}
 }
