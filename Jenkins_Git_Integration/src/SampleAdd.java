2533b9377e84f9c5741bbebc6243d8752c53b393
delarosa0213 <rosteldelarosa02@gmail.com>
2023-05-09 15:22:49 +0800

    updated try catch block
    updating the try catch function

--- Jenkins_Git_Integration/src/SampleAdd.java
+++ Jenkins_Git_Integration/src/SampleAdd.java
@@ -2,36 +2,27 @@ import java.util.Scanner;
 
 public class SampleAdd {
     public static void main(String[] args) {
+        int y = 10;
+        int x = 0;
+        int ans = 0;
+        
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the value of x: ");
-        int x;
         while (true) {
             try {
+                System.out.print("Enter the value of x: ");
                 x = scanner.nextInt();
                 break;
             } catch (Exception e) {
-                System.out.print("Invalid input. Please enter an integer value for x: ");
+                System.out.println("Invalid input. Please enter an integer value for x:");
                 scanner.nextLine();
             }
         }
-
-        System.out.print("Enter the value of y: ");
-        int y;
-        while (true) {
-            try {
-                y = scanner.nextInt();
-                break;
-            } catch (Exception e) {
-                System.out.print("Invalid input. Please enter an integer value for y: ");
-                scanner.nextLine();
-            }
-        }
-
-        int ans = x + y;
-
-        System.out.println("The sum of x and y is: " + ans);
 
         scanner.close();
+
+        ans = x + y;
+
+        System.out.println("x + y = " + ans);
     }
 }
