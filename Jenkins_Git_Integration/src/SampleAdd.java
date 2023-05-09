156af73205c63e4c5d3e3dc6dabce36470687fbf
delarosa0213 <rosteldelarosa02@gmail.com>
2023-05-09 15:33:16 +0800

    remove the infinite loop

--- Jenkins_Git_Integration/src/SampleAdd.java
+++ Jenkins_Git_Integration/src/SampleAdd.java
@@ -4,21 +4,20 @@ public class SampleAdd {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = 0;
-        
+
         // Prompt the user to enter a valid integer for x
         while (true) {
             System.out.print("Enter the value of x: ");
             if (sc.hasNextInt()) {
                 x = sc.nextInt();
+                sc.nextLine(); // consume the newline character left in the input buffer
                 break;
             } else {
                 System.out.println("Invalid input. Please enter an integer value for x:");
-                if (sc.hasNextLine()) {
-                    sc.nextLine();
-                }
+                sc.nextLine(); // consume the entire line to clear the input buffer
             }
         }
-        
+
         int y = 10;
         int sum = x + y;
         System.out.println("Sum of x and y is: " + sum);
