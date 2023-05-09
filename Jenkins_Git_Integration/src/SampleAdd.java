a3d0a187fafac9cd1a208f19a3a0651b0e72423e
delarosa0213 <rosteldelarosa02@gmail.com>
2023-05-09 15:19:49 +0800

    adding try catch
    adding try catch

--- Jenkins_Git_Integration/src/SampleAdd.java
+++ Jenkins_Git_Integration/src/SampleAdd.java
@@ -1,20 +1,37 @@
-
 import java.util.Scanner;
 
 public class SampleAdd {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("Enter the value of x: ");
-		int x = scanner.nextInt();
-		
-		System.out.print("Enter the value of y: ");
-		int y = scanner.nextInt();
-		
-		int ans = x + y;
-		
-		System.out.println("The sum of x and y is: " + ans);
-		
-		scanner.close();
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the value of x: ");
+        int x;
+        while (true) {
+            try {
+                x = scanner.nextInt();
+                break;
+            } catch (Exception e) {
+                System.out.print("Invalid input. Please enter an integer value for x: ");
+                scanner.nextLine();
+            }
+        }
+
+        System.out.print("Enter the value of y: ");
+        int y;
+        while (true) {
+            try {
+                y = scanner.nextInt();
+                break;
+            } catch (Exception e) {
+                System.out.print("Invalid input. Please enter an integer value for y: ");
+                scanner.nextLine();
+            }
+        }
+
+        int ans = x + y;
+
+        System.out.println("The sum of x and y is: " + ans);
+
+        scanner.close();
+    }
 }
