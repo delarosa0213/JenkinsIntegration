2a0e911a776625f33be05b1fe2a8065575117a31
delarosa0213 <rosteldelarosa02@gmail.com>
2023-05-09 15:35:59 +0800

    fix code

--- Jenkins_Git_Integration/src/SampleAdd.java
+++ Jenkins_Git_Integration/src/SampleAdd.java
@@ -2,27 +2,11 @@ import java.util.Scanner;
 
 public class SampleAdd {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int x = 0;
+        int x = 10;
+        int y = 20;
         
-        // Prompt the user to enter a valid integer for x
-        while (true) {
-            System.out.print("Enter the value of x: ");
-            if (sc.hasNextInt()) {
-                x = sc.nextInt();
-                break;
-            } else {
-                System.out.println("Invalid input. Please enter an integer value for x:");
-                if (sc.hasNextLine()) {
-                    sc.nextLine();
-                } else {
-                    break; // exit the loop if there is no more input
-                }
-            }
-        }
+        int ans = x + y;
         
-        int y = 10;
-        int sum = x + y;
-        System.out.println("Sum of x and y is: " + sum);
+        System.out.println();
     }
 }
